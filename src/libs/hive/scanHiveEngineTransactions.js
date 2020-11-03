42f2f0a0b33a546d46828888406bcdb0e59784e1
fbslo <admin@fbslo.net>
2020-11-03 18:21:00 +0100

    add secondary node check

--- src/libs/hive/scanHiveEngineTransactions.js
+++ src/libs/hive/scanHiveEngineTransactions.js
@@ -1,6 +1,11 @@
 const { HiveEngine } = require("@splinterlands/hive-interface")
 const hive_engine = new HiveEngine();
 
+const axios = require('axios');
+const low = require('lowdb')
+const FileAsync = require('lowdb/adapters/FileAsync')
+const adapter = new FileAsync('./mempool.json')
+
 let alreadyProcessed = []
 
 function start(callback){
@@ -16,7 +21,12 @@ function start(callback){
        let txHash = tx.transactionId.split("-")[0]
        if (!alreadyProcessed.includes(txHash)){
          alreadyProcessed.push(txHash)
-         callback(tx)
+         if (process.env.VERIFY_SECONDARY_NODE){
+           let isTransactionValid = getSecondaryNodeInformation(transactionId, tx)
+           if (isTransactionValid == 'transaction_valid') callback(tx)
+         } else {
+           callback(tx)
+         }
        }
      }
    });
@@ -27,6 +37,65 @@ function start(callback){
  }
 }
 
-start()
+function getSecondaryNodeInformation(transactionId, tx){
+  return new Promise(async (resolve, reject) => {
+    axios.post(process.env.HIVE_ENGINE_SECONDARY_ENDPOINT, {
+      "jsonrpc": "2.0",
+      "method": "getTransactionInfo",
+      "params": {
+        "txid": transactionId
+      },
+      "id": 1
+    })
+    .then(function (response) {
+      if (response == null || response.data == null || response.data.result == null) {
+        //add transaction to mempool
+        low(adapter)
+          .then(db => {
+            db.defaults({ transactions: [] }).write()
+
+            let isAlreadyInMemPool = db.get("transactions").find({ id: transactionId }).value()
+            if (isAlreadyInMemPool.length == 0){
+              db.get('transactions')
+                .push({ id: transactionId, tx: tx })
+                .write()
+              resolve("added_to_mempool")
+            } else {
+              resolve("already_in_mempool")
+            }
+          })
+      }
+      else if (!response.data.result.logs.includes("errors")){
+        resolve("transaction_valid")
+      } else {
+        reject("transaction_rejected")
+      }
+    })
+    .catch(function (error) {
+      reject(err)
+    });
+  })
+}
+
+async function checkMempool(){
+  return new Promise((resolve, reject) => {
+    low(adapter)
+      .then(async db => {
+        try {
+          db.defaults({ transactions: [] }).write()
+          let mempool = db.get("transactions").value()
+          for (i in mempool){
+            let isValid = await getSecondaryNodeInformation(mempool[i].id)
+            if (isValid == "transaction_valid"){
+              resolve(mempool[i].id)
+            }
+          }
+        } catch (e) {
+          reject(e)
+        }
+      })
+  })
+}
 
 module.exports.start = start
+module.exports.checkMempool = checkMempool
